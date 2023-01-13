d93efb8fdfc7d13a735306a21d2a5c5158fd5d9d
anagigoncalves <ana.gigoncalves@gmail.com>
2023-01-13 17:11:48 +0000

    understanding git stash

--- preprocess_tiff.py
+++ preprocess_tiff.py
@@ -10,7 +10,7 @@ import glob
 import os
 import matplotlib.pyplot as plt
 
-path = 'D:\\TM RAW FILES\\split contra fast\\MC9194\\2021_07_02\\'
+path = 'D:\\TM RAW FILES\\tied baseline\\MC9194\\2021_07_03\\'
 delim = path[-1]
 if delim == '/':
     path_output = path+'/Suite2p'
